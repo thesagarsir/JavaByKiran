9981b729d63b91fc5c8a9bbad624cdc914e2a636
vidhya-jagdale <vidhyanikam2010@gmail.com>
2020-11-26 22:27:43 +0530

    AT:changes done
    Signed-off-by: vidhya-jagdale <vidhyanikam2010@gmail.com>

--- SpringBootHBTest/src/main/java/com/jbs/SpringBootHBTest/controller/SpringController.java
+++ SpringBootHBTest/src/main/java/com/jbs/SpringBootHBTest/controller/SpringController.java
@@ -18,6 +18,7 @@ public class SpringController {
 	@GetMapping("getallemployee")
 	public String getAllEmpolee() {
 		System.out.println("helloHi vidhya");
+		System.out.println("satara");
 		//List<Demo> listOfEmp = springService.getAllEmpolee();
 		//return listOfEmp;
 		
