4b6673f4c86bf02183bc4beb6ca889fb42c659fc
Nitin <nj338023@gmail.com>
2020-11-28 11:02:32 +0530

    NJ: my commit
    Signed-off-by: Nitin <nj338023@gmail.com>

--- SpringBootHBTest/src/main/java/com/jbs/SpringBootHBTest/controller/SpringController.java
+++ SpringBootHBTest/src/main/java/com/jbs/SpringBootHBTest/controller/SpringController.java
@@ -17,10 +17,10 @@ public class SpringController {
 	
 	@GetMapping("getallemployee")
 	public String getAllEmpolee() {
-		System.out.println("helloHi vidhya");
-		System.out.println("satara");
+	
 		//List<Demo> listOfEmp = springService.getAllEmpolee();
 		//return listOfEmp;
+		System.out.println("My Changes");
 		
 		return "hi";
 	}
